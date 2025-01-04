1f6d0b8a0e8dbfa7590c66ee0795e331307ccb58
Omid Jaberi <omid.jaberi@sw-motion.cn>
2025-01-04 17:31:36 +0330

    stars refactored

--- stars.js
+++ stars.js
@@ -1,36 +1,35 @@
 export class Stars {
+  static speed = 1e-3;
   static ctx;
   constructor(cnt, width, height, start = -1) {
     this.width = width;
     this.height = height;
-    this.cnt = cnt;
+    this.starCount = cnt;
     this.create(start);
   }
   create(start = -1) {
-    this.arr = [];
+    this.stars = [];
     this.life = start;
-    for (let i = 0; i < this.cnt; i++)
-      this.arr.push({
-        'x': parseInt(Math.random() * this.width),
-        'y': parseInt((Math.random() + start) * this.height),
+    for (let i = 0; i < this.starCount; i++)
+      this.stars.push({
+        'x': Math.random(),
+        'y': Math.random() + start,
         'phase': Math.random(),
         'freq': Math.random() * 0.4 + 0.4
       });
   }
   draw() {
-    for (let i = 0; i < this.cnt; i++) {
-      let opac = Math.abs(this.arr[i].phase * 1000 + parseInt(Date.now() * this.arr[i].freq) % 1000 - 500) / 500;
-      Stars.ctx.fillStyle = `rgba(255, 255, 255, ${opac})`;
-      Stars.ctx.fillRect(this.arr[i].x, this.arr[i].y, 1, 1);
-    }
+    this.stars.forEach(star => {
+      let opacity = Math.abs(star.phase * 1000 + parseInt(Date.now() * star.freq) % 1000 - 500) / 500;
+      Stars.ctx.fillStyle = `rgba(255, 255, 255, ${opacity})`;
+      Stars.ctx.fillRect(parseInt(star.x * this.width), parseInt(star.y * this.height), 1, 1);
+    });
     this.update();
   }
   update() {
-    this.life += 0.001;
+    this.life += Stars.speed;
     if (this.life < 1)
-      for (let i = 0; i < this.arr.length; i++) {
-        this.arr[i].y += 0.001 * this.height;
-      }
+      this.stars.forEach(star => { star.y += Stars.speed; });
     else
       this.create();
   }
